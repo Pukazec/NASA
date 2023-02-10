c87a0c13069e6b0639b2301e39bf49cf10894579
Pukazec <skvorc.leo@gmail.com>
2023-02-10 21:47:22 +0100

    Dark theme preference

--- app/src/main/java/leo/skvorc/nasa/SettingsFragment.kt
+++ app/src/main/java/leo/skvorc/nasa/SettingsFragment.kt
@@ -5,10 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.preference.PreferenceFragmentCompat
+import androidx.appcompat.app.AppCompatDelegate
+import leo.skvorc.nasa.databinding.FragmentSettingsBinding
 
-class SettingsFragment : PreferenceFragmentCompat() {
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        addPreferencesFromResource(R.xml.prefs)
+class SettingsFragment : Fragment() {
+    private lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        binding.theme.setOnClickListener {
+            if (binding.theme.isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+        return binding.root
     }
 }
